73d5f12eb03316aead75eebc3591849b16a49712
Kirill Krakov <kirill170251@gmail.com>
2024-10-01 23:53:55 +0300

    update task 4 test

--- task4/test/task4_test.exs
+++ task4/test/task4_test.exs
@@ -1,24 +1,24 @@
 defmodule Task4Test do
   use ExUnit.Case
 
-  import TailRecursion
-  import UsualRecursion
-  import Modular
-  import Lazy
+  doctest TailRecursion
+  doctest UsualRecursion
+  doctest Modular
+  doctest Lazy
 
   test "Tail Recursion" do
-    assert largest_palindrome_tail_recur() == 906609
+    assert TailRecursion.largest_palindrome_tail_recur() == 906609
   end
 
   test "Usual Recursion" do
-    assert largest_palindrome_usual_recur() == 906609
+    assert UsualRecursion.largest_palindrome_usual_recur() == 906609
   end
 
   test "Modular" do
-    assert largest_palindrome_modular() == 906609
+    assert Modular.largest_palindrome_modular() == 906609
   end
 
   test "Lazy" do
-    assert largest_palindrome_lazy() == 906609
+    assert Lazy.largest_palindrome_lazy() == 906609
   end
 end
